a511bf315748a0a5e81a656257383d46716eaf6f
Keita Miura <keita.miura@tier4.jp>
2023-03-28 01:29:48 +0900

    delete info

--- planning/automatic_emergency_braking/src/automatic_emergency_braking_node.cpp
+++ planning/automatic_emergency_braking/src/automatic_emergency_braking_node.cpp
@@ -8,7 +8,6 @@ AutomaticEmergencyBrakingNode::AutomaticEmergencyBrakingNode(const rclcpp::NodeO
 {
   RCLCPP_INFO(this->get_logger(), "Template launched.");
   auto & p = param_;
-  RCLCPP_INFO(rclcpp::get_logger("my_node"), "LINE %d", __LINE__);
 
   p.min = declare_parameter("min", 0.0);
   p.max = declare_parameter("max", 3.0);
@@ -16,30 +15,24 @@ AutomaticEmergencyBrakingNode::AutomaticEmergencyBrakingNode(const rclcpp::NodeO
 	sub_stop_reasons_ = this->create_subscription<tier4_planning_msgs::msg::StopReasonArray>(
 		"~/input/stop_reasons", 1,
 		std::bind(&AutomaticEmergencyBrakingNode::subStopReasons, this, std::placeholders::_1));
-  RCLCPP_INFO(rclcpp::get_logger("my_node"), "LINE %d", __LINE__);
 
   sub_current_twist_ = this->create_subscription<Odometry>(
     "~/input/odometry", 1,
     std::bind(&AutomaticEmergencyBrakingNode::currentTwistCallback, this, std::placeholders::_1));
-  RCLCPP_INFO(rclcpp::get_logger("my_node"), "LINE %d", __LINE__);
 
   sub_joy_status_ = this->create_subscription<sensor_msgs::msg::Joy>(
 		"~/input/joy", 1,
 		std::bind(&AutomaticEmergencyBrakingNode::joyStatusCallback, this, std::placeholders::_1));
-  RCLCPP_INFO(rclcpp::get_logger("my_node"), "LINE %d", __LINE__);
   
 
   sub_whill_odom_ = this->create_subscription<Odometry>(
     "~/input/whill_odom",
     rclcpp::QoS(1),
     std::bind(&AutomaticEmergencyBrakingNode::callbackWhillOdom, this, std::placeholders::_1));
-  RCLCPP_INFO(rclcpp::get_logger("my_node"), "LINE %d", __LINE__);
-  RCLCPP_INFO(rclcpp::get_logger("my_node"), "LINE %d", __LINE__);
 
 
   // set Publisher
 	pub_control_status_ = this->create_publisher<sensor_msgs::msg::Joy>("~/output/vehicle_cmd", rclcpp::QoS(1));
-  RCLCPP_INFO(rclcpp::get_logger("my_node"), "LINE %d", __LINE__);
 
   pub_velocity_status_ = this->create_publisher<AwVelocity>("~/output/aw_velocity", 1);
   AwVelocity set_vel;
@@ -49,17 +42,14 @@ AutomaticEmergencyBrakingNode::AutomaticEmergencyBrakingNode(const rclcpp::NodeO
   set_vel.header.frame_id = "base_link";
   set_vel.header.stamp = Node::now();
   pub_velocity_status_->publish(set_vel);
-  RCLCPP_INFO(rclcpp::get_logger("my_node"), "LINE %d", __LINE__);
 
   pub_steering_status_ = this->create_publisher<AwSteering>("~/output/aw_steering", 1);
   AwSteering set_steer;
   set_steer.steering_tire_angle = 0.0;
   set_steer.stamp = Node::now();
   pub_steering_status_->publish(set_steer);
-  RCLCPP_INFO(rclcpp::get_logger("my_node"), "LINE %d", __LINE__);
 
 	pub_stop_reason = this->create_publisher<tier4_planning_msgs::msg::StopReasonArray>("~/output/stop_reson", rclcpp::QoS(1));
-  RCLCPP_INFO(rclcpp::get_logger("my_node"), "LINE %d", __LINE__);
 
 }
 
@@ -74,7 +64,6 @@ void AutomaticEmergencyBrakingNode::currentTwistCallback(const Odometry::ConstSh
 void AutomaticEmergencyBrakingNode::subStopReasons(
   const tier4_planning_msgs::msg::StopReasonArray::ConstSharedPtr msg)
 {
-  RCLCPP_INFO(rclcpp::get_logger("my_node"), "LINE %d", __LINE__);
   tier4_planning_msgs::msg::StopReasonArray output = *msg;
   pub_stop_reason->publish(output);
   std::shared_ptr<tier4_planning_msgs::msg::StopReason> reason_for_obstacle;
@@ -83,14 +72,12 @@ void AutomaticEmergencyBrakingNode::subStopReasons(
   for (auto stop_reason : msg->stop_reasons){
   RCLCPP_INFO(rclcpp::get_logger("my_node"), "LINE %d %s", __LINE__, stop_reason.reason.c_str());
     if (stop_reason.reason == "ObstacleStop" && stop_reason.stop_factors.size() != 0){
-  RCLCPP_INFO(rclcpp::get_logger("my_node"), "LINE %d", __LINE__);
       reason_for_obstacle = std::make_shared<tier4_planning_msgs::msg::StopReason>(stop_reason);
 			// find_obstacle = true;
     }
   }
 	// if (!find_obstacle){
   if (!reason_for_obstacle){
-  RCLCPP_INFO(rclcpp::get_logger("my_node"), "LINE %d", __LINE__);
 		return;
   }
   if (reason_for_obstacle->stop_factors[0].stop_factor_points.size() == 0){
@@ -105,7 +92,6 @@ void AutomaticEmergencyBrakingNode::subStopReasons(
   
   RCLCPP_INFO(rclcpp::get_logger("my_node"), "LINE %f", distance);
   if (distance > param_.min && distance < param_.max) {
-  RCLCPP_INFO(rclcpp::get_logger("my_node"), "LINE %d", __LINE__);
     emagency_stop = true;
   }
   else {
@@ -118,7 +104,6 @@ void AutomaticEmergencyBrakingNode::callbackWhillOdom(const Odometry::ConstShare
 {
   AwSteering steer_angle;
   AwVelocity velocity;
-  // RCLCPP_INFO(rclcpp::get_logger("my_node"), "LINE %d", __LINE__);
 
   double linear = msg->twist.twist.linear.x;
   double angular = msg->twist.twist.angular.z;
@@ -141,7 +126,6 @@ void AutomaticEmergencyBrakingNode::callbackWhillOdom(const Odometry::ConstShare
   // steer_angle.stamp = msg->header.stamp;
   // velocity.header = msg->header;
   // RCLCPP_INFO(this->get_logger(),"velocity:%f, angle:%f", velocity.longitudinal_velocity, steer_angle.steering_tire_angle);
-  // RCLCPP_INFO(rclcpp::get_logger("my_node"), "LINE %d", __LINE__);
   pub_steering_status_->publish(steer_angle);
   pub_velocity_status_->publish(velocity);
   return;
@@ -149,7 +133,6 @@ void AutomaticEmergencyBrakingNode::callbackWhillOdom(const Odometry::ConstShare
 
 void AutomaticEmergencyBrakingNode::joyStatusCallback(const sensor_msgs::msg::Joy msg)
 {
-  // RCLCPP_INFO(rclcpp::get_logger("my_node"), "LINE %d", __LINE__);
   auto joy_ = msg;
   controlWhillVehicle(joy_);
 }
@@ -157,7 +140,6 @@ void AutomaticEmergencyBrakingNode::joyStatusCallback(const sensor_msgs::msg::Jo
 void AutomaticEmergencyBrakingNode::controlWhillVehicle(sensor_msgs::msg::Joy joy_)
 {
   if(emagency_stop){
-  RCLCPP_INFO(rclcpp::get_logger("my_node"), "LINE %d", __LINE__);
     joy_.axes[0] = 0.0; //　Longitudinal direction
     joy_.axes[1] = 0.0; //　horizon direction
   }
